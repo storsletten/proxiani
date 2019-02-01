86ae1f06f0bc0fb3523840966105339ee6a892a8
tms88 <tms88@users.noreply.github.com>
2019-02-01 22:34:12 +0100

    Add script for creating changelog entries

---
+++ Commit.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+mode con: cols=150
+title Commit a new change
+timeout /t 1 >nul
+
+set "NewCommitsFile=%~dp0\New commits.txt"
+
+echo Enter a message for this commit.
+echo Note: Do not use quotation marks^^!
+set /p "CommitMessage=Message: "
+
+if "!CommitMessage!"=="" (
+ echo An empty string is not a valid message.
+ pause
+ exit /b 1
+)
+
+(echo * !CommitMessage!) >"%NewCommitsFile%"
+
+git add .
+git commit -m "!CommitMessage!"
+
+echo Done^^!
+pause
--- Publish.bat
+++ Publish.bat
@@ -6,14 +6,21 @@ timeout /t 1 >nul
 
 set "devDir=%~dp0"
 set "pubDir=%devDir%..\Miriani Soundpack for VIP Mud\Proxiani\"
+set "ChangelogFile=%~dp0\CHANGELOG.txt"
+set "NewCommitsFile=%~dp0\New commits.txt"
 
 if not exist "%pubDir%" (
  echo Public directory not found.
  pause
  exit /b 1
 )
+if not exist "%NewCommitsFile%" (
+ echo No new commits.
+ pause
+ exit /b 1
+)
 
-echo Enter new version string or which part of the version string to bump.
+echo Enter a version bumping keyword.
 echo Example: patch
 echo Empty input defaults to patch.
 set /p "InputString=Version: "
@@ -25,7 +32,17 @@ if "!InputString!"=="" (
 
 npm version !NewRelease! && npm publish && git push --tags
 
+for /f "tokens=* USEBACKQ" %%a in (`git describe "--abbrev=0"`) do set tag=%%a
+echo Version %tag:~1%, released %date%:>"%ChangelogFile%.tmp"
+type "%NewCommitsFile%" >>"%ChangelogFile%.tmp"
+echo.>>"%ChangelogFile%.tmp"
+type "%ChangelogFile%" >>"%ChangelogFile%.tmp"
+move /y "%ChangelogFile%.tmp" "%ChangelogFile%" >nul
+del /q "%NewCommitsFile%" >nul
+
 xcopy "!devDir!middleware" "!pubDir!middleware" /D /E /Q /V /M /Y>nul
 xcopy "!devDir!src" "!pubDir!src" /D /E /Q /V /M /Y>nul
+xcopy "!devDir!CHANGELOG.txt" "!pubDir!CHANGELOG.txt" /D /Q /V /M /Y>nul
 xcopy "!devDir!package.json" "!pubDir!package.json" /D /Q /V /M /Y>nul
+
 pause
