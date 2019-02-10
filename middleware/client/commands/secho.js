0f6519b47aff632628a24927e7335e10c5f9b937
tms88 <tms88@users.noreply.github.com>
2019-02-10 16:49:10 +0100

    Fixed a bug that prevented you from aborting the conf command.

--- middleware/client/commands/secho.js
+++ middleware/client/commands/secho.js
@@ -3,7 +3,11 @@ const secho = (data, middleware) => {
  data.respond.push(`What would you like to be repeated back to you using soundpack echo?`, `[Type a line of input or \`@abort' to abort the command.]`);
  middleware.setState('secho', (data, middleware) => {
   if (middleware.isOOB(data.input)) return 0;
-  data.respond.push(`${middleware.device.oob}soundpack echo | ${data.forward.pop()}`);
+  else if (data.input.trim().toLowerCase() === '@abort') {
+   data.forward.pop();
+   data.respond.push(`>> Command Aborted <<`);
+  }
+  else data.respond.push(`${middleware.device.oob}soundpack echo | ${data.forward.pop()}`);
  }).timeout = 0;
 };
 
