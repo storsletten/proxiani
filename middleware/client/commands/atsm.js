0bc8b49aad50901404325c040c1774a9828014b2
tms88 <tms88@users.noreply.github.com>
2019-02-16 14:41:38 +0100

    More atsm improvements. Better performance, better results, and better alerts.

--- middleware/client/commands/atsm.js
+++ middleware/client/commands/atsm.js
@@ -1,36 +1,70 @@
 const utils = require('../../../src/utils');
 const direction = require('../../helpers/direction');
 
-const findBestObjects = (current, objects, goal, ratioBreaker = 0, expandProbingDistance = 0, step = 0) => {
+const findBestObjects = (current, objects, goal, expandProbingDistance = 0, step = 0, gettingWorse = 0) => {
  goal--;
  step++;
  if (step > 1) objects = objects.map(object => { return { ...object, distance: Math.max(Math.abs(current.x - object.x), Math.abs(current.y - object.y)) }; }).sort((a, b) => a.distance - b.distance);
  else {
-  current = { ...current, totalCount: 0, totalDistance: 0 };
+  current = { ...current, totalCount: 0, totalDistance: 0, ratio: 0 };
   objects = objects.map(object => { return { ...object }; });
  }
  const nearest = objects[0];
- if (objects.length === 1 || goal < 1) return { ...nearest, totalCount: nearest.count, totalDistance: nearest.distance, ratio: nearest.count / nearest.distance };
+ if (objects.length === 1 || goal < 1 || gettingWorse > 2 || (gettingWorse > 0 && current.totalDistance > 15)) return { ...nearest, totalCount: nearest.count, totalDistance: nearest.distance, ratio: nearest.count / nearest.distance };
  const maxProbingDistance = nearest.distance + expandProbingDistance;
  const badObjectsIndex = objects.findIndex(object => object.distance > maxProbingDistance);
- return (badObjectsIndex > 0 ? objects.slice(0, badObjectsIndex) : objects).map((object, index) => {
-  const totalCount = current.totalCount + object.count;
-  const totalDistance = current.totalDistance + object.distance;
+ const bestObject = (badObjectsIndex > 0 ? objects.slice(0, badObjectsIndex) : objects).map((object, index) => {
+  let totalCount = current.totalCount + object.count;
+  let totalDistance = current.totalDistance + object.distance;
   const ratio = totalCount / totalDistance;
-  const next = findBestObjects({ x: object.x, y: object.y, totalCount, totalDistance }, [ ...objects.slice(0, index), ...objects.slice(index + 1) ], goal, ratioBreaker, expandProbingDistance, step);
-  if (next.ratio < ratioBreaker && ratio > next.ratio) return { ...object, totalCount: object.count, totalDistance: object.distance, ratio: object.count / object.distance };
-  else {
-   const totalCount = next.totalCount + object.count;
-   const totalDistance = next.totalDistance + object.distance;
-   return { ...object, totalCount, totalDistance, ratio: totalCount / totalDistance, next };
-  }
+  const next = findBestObjects({
+   x: object.x,
+   y: object.y,
+   totalCount,
+   totalDistance,
+  }, [
+   ...objects.slice(0, index),
+   ...objects.slice(index + 1),
+  ], goal, expandProbingDistance, step, ratio < current.ratio ? gettingWorse + 1 : 0);
+  totalCount = next.totalCount + object.count;
+  totalDistance = next.totalDistance + object.distance;
+  return {
+   ...object,
+   totalCount,
+   totalDistance,
+   ratio: totalCount / totalDistance,
+   next,
+  };
  }).sort((a, b) => (b.ratio - a.ratio) || (a.distance - b.distance))[0];
+ return bestObject;
 };
 
-const ratioAssessment = ratio => {
- if (ratio >= 0.5) return 'Good';
- else if (ratio > 0.33) return 'Acceptable';
- else return 'Bad';
+const getAssessment = (object, max = 0) => {
+ const assessmentLabels = ['Good', 'Acceptable', 'Bad'];
+ const assess = ratio => {
+  if (ratio >= 0.5) return 0;
+  else if (ratio > 0.33) return 1;
+  else return 2;
+ };
+ let totalCount = object.count;
+ let totalDistance = object.distance;
+ let ratio = totalCount / totalDistance;
+ let assessment = assess(ratio);
+ while (max !== 1 && object.next) {
+  const next = object.next;
+  const latestTotalCount = totalCount + next.count;
+  const latestTotalDistance = totalDistance + next.distance;
+  const latestRatio = latestTotalCount / latestTotalDistance;
+  const latestAssessment = assess(latestRatio);
+  if (latestAssessment > assessment) break;
+  assessment = latestAssessment;
+  totalCount = latestTotalCount;
+  totalDistance = latestTotalDistance;
+  ratio = latestRatio;
+  object = next;
+  max--;
+ }
+ return { totalCount, totalDistance, ratio, label: assessmentLabels[assessment], level: assessment };
 };
 
 const parseObjects = (text, currentCoordinates) => {
@@ -109,42 +143,47 @@ const atsm = (data, middleware, linkedMiddleware) => {
   const state = middleware.states.atsm.data;
   if (reader(data, state)) return state.reading ? 1 : 0;
   if (!state.readingComplete) return 0b10;
-  const powerForScooping = state.power - ((state.distance * 3) + 2);
+  const sparePower = state.power - ((state.distance - 1) * 3);
+  const powerForScooping = sparePower - 5;
   const objects = state.objects;
   objects.forEach(object => object.distance = Math.max(Math.abs(state.coordinates.x - object.x), Math.abs(state.coordinates.y - object.y)));
   objects.sort((a, b) => a.distance !== b.distance ? a.distance - b.distance : b.count - a.count);
   const scoopedObject = objects.length > 0 && objects[0].distance === 0 ? objects.shift() : undefined;
   if (scoopedObject) state.cargo += scoopedObject.count;
   state.cargoSpace = state.cargoCapacity - state.cargo;
+  if (sparePower <= 0) data.forward.push(`Critical power level: Currently ${sparePower}% power for returning to the ship.`);
   if (objects.length === 0) data.forward.push(scoopedObject ? 'No more objects.' : 'No objects.');
   else if (state.cargoSpace <= 0) data.forward.push(`No more cargo space.`);
   else {
    let bestObject;
    if (state.goal !== undefined || (powerForScooping > objects[0].distance && state.cargo < state.cargoCapacity)) {
     if (state.goal === undefined) {
-     const goal = Math.max(1, Math.min(6, state.cargoSpace, objects.length));
+     const goal = Math.max(1, Math.min(10, state.cargoSpace, objects.length));
      state.extraProbingDistance = Math.min(state.extraProbingDistance, Math.max(0, state.maxPreferredDistance - objects[0].distance));
-     bestObject = findBestObjects(state.coordinates, objects, goal, state.ratioBreaker, state.extraProbingDistance);
+     bestObject = findBestObjects(state.coordinates, objects, goal, state.extraProbingDistance);
     }
-    else bestObject = findBestObjects(state.coordinates, objects, state.goal, 0, state.extraProbingDistance);
+    else bestObject = findBestObjects(state.coordinates, objects, state.goal, state.extraProbingDistance);
    }
    else bestObject = findBestObjects(state.coordinates, objects, 1);
-   if (bestObject.distance > powerForScooping) data.forward.push(`Low power!`);
-   data.forward.push(`${direction.calculate2d(state.coordinates, bestObject).dir}.`);
-   if (bestObject.ratio) {
-    const count = bestObject.totalCount || bestObject.count;
-    const distance = bestObject.totalDistance || bestObject.distance;
-    if (count > 1) data.forward.push(`${bestObject.distance < 3 ? 'Good' : ratioAssessment(bestObject.ratio)}: ${utils.formatAmount(count, 'object')} in ${utils.formatAmount(distance, 'unit')}.`);
-    else data.forward.push(`${ratioAssessment(bestObject.ratio)}.`);
+   if (sparePower > 0 && bestObject.distance > powerForScooping) data.forward.push(`Low power!`);
+   const assessment = getAssessment(bestObject);
+   const nearestAssessment = getAssessment(bestObject, 3);
+   if (nearestAssessment.label === 'Bad' && assessment.label !== 'Good') {
+    const dir = (state.distance === 1 && powerForScooping > 6 && powerForScooping >= bestObject.distance) || ((powerForScooping / state.distance) > 6 && (state.distance === 1 || (state.cargoSpace / 3) >= state.distance)) ? 'down' : 'up';
+    data.forward.push(`${assessment.label === 'Bad' ? 'Go' : 'Could go'} ${dir}.`);
+   }
+   data.forward.push(`${nearestAssessment.label}: ${direction.calculate2d(state.coordinates, bestObject).dir}.`);
+   if (assessment.totalCount > 1) {
+    data.forward.push(`${assessment.label !== nearestAssessment.label ? `${assessment.label}: ` : ''}${utils.formatAmount(assessment.totalCount, 'object')} in ${utils.formatAmount(assessment.totalDistance, 'unit')}.`);
    }
   }
-  data.forward.push(`${powerForScooping}% power for ${state.cargoSpace > 0 ? 'scooping' : 'returning to the ship'}.`);
+  if (state.cargoSpace > 0) data.forward.push(`${powerForScooping}% power for scooping.`);
+  else if (sparePower > 0) data.forward.push(`${sparePower}% excess power.`);
   data.forward.push(`Cargo ${state.cargo} of ${state.cargoCapacity}.`);
   data.forward.push(`Ship is ${state.distance} unit${state.distance !== 1 ? 's' : ''} distant.`);
   data.forward.push(`Total ${objects.length} object${objects.length !== 1 ? 's' : ''}.`);
  }, {
   goal: data.command.length > 1 && isFinite(data.command[1]) ? Math.max(0, Math.floor(Number(data.command[1]))) : undefined,
-  ratioBreaker: 0.35,
   extraProbingDistance: 2,
   maxPreferredDistance: 4,
  });
