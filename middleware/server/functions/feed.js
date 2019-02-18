1a63cbe49b019c3aaa1e6de6cb3c52596916c456
tms88 <tms88@users.noreply.github.com>
2019-02-18 20:33:41 +0100

    Added server middleware for feeds.

---
+++ middleware/server/functions/feed.js
@@ -0,0 +1,25 @@
+module.exports = (data, middleware, linkedMiddleware) => {
+ if (data.input.length === 0 || data.input.startsWith('#$#')) return false;
+ if (data.input[0] === '(' || data.input.startsWith('[From Outside]')) {
+  const feeds = [];
+  let str = data.input;
+  while (feeds.length < 10) {
+   if (str.startsWith('[From Outside]')) {
+    feeds.push('From Outside');
+    str = str.slice(15);
+   }
+   else {
+    const m = str.match(/^\(([^)]+)\) /);
+    if (m) {
+     feeds.push(m[1]);
+     str = str.slice(m[1].length + 3);
+    }
+    else break;
+   }
+  }
+  if (feeds.length > 0) {
+   if (feeds.length > 1 || feeds[0] !== 'From Outside') data.forward[0] = `(${feeds.join(' | ')}) ${str}`;
+   return true;
+  }
+ }
+};
