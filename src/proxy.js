79c29b948de2b3c2cfff231e20b51813e6a9931c
tms88 <tms88@users.noreply.github.com>
2019-02-21 08:55:58 +0100

    Fix px restart not working

--- src/proxy.js
+++ src/proxy.js
@@ -35,7 +35,7 @@ class Proxy {
    if (this.restartRequested) {
     this.console(`Restarting Proxiani...`);
     for (let mod in require.cache) delete require.cache[mod];
-    setTimeout(() => require(__filename), 50);
+    setTimeout(() => require('./main'), 50);
    }
    else this.console(`Shutting down Proxiani`);
    delete this.events;
