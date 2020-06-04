b74f767892cfb7bf96c606d2f877698038fb23eb
tms88 <tms88@users.noreply.github.com>
2020-06-04 11:49:07 +0200

    Encoding of extended ASCII now uses %& instead of only % for declaring an encoded character. This should reduce the risk of unintentional encoding/decoding. It also means that old versions of Proxiani are not compatible with encoded characters from newer versions of Proxiani starting with this one, and vice versa.

--- src/filters.js
+++ src/filters.js
@@ -52,7 +52,7 @@ const encode = (data, { lowFilter = 0, highFilter = 1, mapSpecial = 1 } = {}) =>
   else continue;
   if (start !== i) buffers.push(data.slice(start, i));
   start = i + 1;
-  if (mode === 1) buffers.push(Buffer.from(`%${data[i].toString(16)}`));
+  if (mode === 1) buffers.push(Buffer.from(`%&${data[i].toString(16)}`));
  }
  if (start !== data.length) buffers.push(data.slice(start));
  return buffers.length === 1 ? buffers[0] : Buffer.concat(buffers);
@@ -63,13 +63,15 @@ const decode = (data, { lowFilter = 2, highFilter = 1 } = {}) => {
  let mode;
  let start = 0;
  let i = 0;
- while ((i = data.indexOf(37, i)) !== -1 && (i + 2) < data.length) {
+ while ((i = data.indexOf(37, i)) !== -1 && (i + 3) < data.length) {
   const end = i;
   let v = data[++i];
+  if (v !== 38) continue;
+  v = data[++i];
   if (v < 48 || v > 102 || (v > 57 && v < 97)) continue;
   v = data[++i];
   if (v < 48 || v > 102 || (v > 57 && v < 97)) continue;
-  v = parseInt(data.slice(end + 1, ++i), 16);
+  v = parseInt(data.slice(end + 2, ++i), 16);
   if (v < 32) mode = lowFilter;
   else if (v > 127) mode = highFilter;
   else mode = 0;
